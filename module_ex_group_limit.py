cb569b93fc822ba78fc2f09667fc53b363e1b388
Stas <113462863+shamansk1y@users.noreply.github.com>
2022-12-13 20:35:10 +0100

    Delete module_ex_group_limit.py

--- module_ex_group_limit.py
+++
@@ -1,8 +0,0 @@
-class InvalidGroupListError(Exception):
-    """Raised when a self.__students list is full"""
-    def __init__(self, max_limit):
-        self.max_limit = max_limit
-
-    def __str__(self):
-        return f'Group full, limit {self.max_limit} students.'
-
