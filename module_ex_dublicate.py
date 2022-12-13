bf5e1723a4cdfb869fc1673c7825343bb57ce534
Stas <113462863+shamansk1y@users.noreply.github.com>
2022-12-13 20:35:25 +0100

    Delete module_ex_dublicate.py

--- module_ex_dublicate.py
+++
@@ -1,8 +0,0 @@
-class DublicateStudentError(Exception):
-    """Raised when a group_title already have this student"""
-    def __init__(self, student, group_title):
-        self.student = student
-        self.group_title = group_title
-
-    def __str__(self):
-        return f'The {self.student} registered in group {self.group_title}.'
